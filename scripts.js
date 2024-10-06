59de30cfa98619d4c9eea43524e2aa06fc350ab8
Gustavo Simão | GS.dev <gs14.dev@gmail.com>
2024-10-05 21:04:49 -0300

    Capturando item
    Necessário rever aula

--- scripts.js
+++ scripts.js
@@ -1,3 +1,49 @@
 console.log ("Olá mundo!")
 
-const item = document.getElementById("input-item")
+const item = document.getElementById("input-item"); 
+const botaoSalvarItem = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+botaoSalvarItem.addEventListener("click", adicionarItem);
+
+function adicionarItem(evento) {
+    evento.preventDefault()
+
+    const itemDaLista = document.createElement("li");
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("lista-item-container");
+
+    const containerNomeDoItem = document.createElement("div");
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.innerText = item.Value;
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("item-lista-button");
+
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";
+    imagemRemover.alt = "Remover";
+
+    botaoRemover.appendChild(imagemRemover);
+    containerBotoes.appendChild(botaoRemover);
+
+    //Aqui foi onde deu merda, refazer a aula de criação de elementos
+
+    //const containerBotoes = document.createElement("div");
+    //const botaoEditar = document.createElement("button");
+    //botaoEditar.classList.add("item-lista-button");
+
+    //const imagemEditar = document.createElement("img");
+    //imagemEditar.src = "img/Edit.svg";
+    //imagemEditar.alt = "Editar";
+
+    //botaoEditar.appendChild(imagemEditar);
+    //containerBotoes.appendChild(botaoEditar);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes)
+    itemDaLista.appendChild(containerItemLista);
+    listaDeCompras.appendChild(itemDaLista);
+}
